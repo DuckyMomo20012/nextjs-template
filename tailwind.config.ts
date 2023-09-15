23fa9723faf687e922111eafd33d9a5ac9b40d00
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-09-15 21:50:59 +0700

    feat: setup shadcn

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,35 +1,77 @@
-import { DEFAULT_THEME as mantineDefaultTheme } from '@mantine/core';
-import type { Config } from 'tailwindcss';
-import twDefaultColors from 'tailwindcss/colors';
-
-// Don't override WindiCSS colors
-const convertColor = (mantineColors, windiColors) => {
-  const newColorPalette = {};
-  Object.keys(mantineColors).forEach((colorName) => {
-    if (windiColors[colorName] instanceof Object === false) {
-      const newColor = {};
-      mantineColors[colorName].forEach((_colorHex, index) => {
-        newColor[`${index * 100}`] = mantineColors[colorName][index];
-      });
-      newColor['50'] = newColor['0'];
-      delete newColor['0'];
-
-      newColorPalette[colorName] = newColor;
-    }
-  });
-
-  return newColorPalette;
-};
-
-export default {
-  content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
-  darkMode: 'class',
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: ['class'],
+  content: [
+    './pages/**/*.{ts,tsx}',
+    './components/**/*.{ts,tsx}',
+    './app/**/*.{ts,tsx}',
+    './src/**/*.{ts,tsx}',
+  ],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
     extend: {
       colors: {
-        ...convertColor(mantineDefaultTheme.colors, twDefaultColors),
+        border: 'hsl(var(--border))',
+        input: 'hsl(var(--input))',
+        ring: 'hsl(var(--ring))',
+        background: 'hsl(var(--background))',
+        foreground: 'hsl(var(--foreground))',
+        primary: {
+          DEFAULT: 'hsl(var(--primary))',
+          foreground: 'hsl(var(--primary-foreground))',
+        },
+        secondary: {
+          DEFAULT: 'hsl(var(--secondary))',
+          foreground: 'hsl(var(--secondary-foreground))',
+        },
+        destructive: {
+          DEFAULT: 'hsl(var(--destructive))',
+          foreground: 'hsl(var(--destructive-foreground))',
+        },
+        muted: {
+          DEFAULT: 'hsl(var(--muted))',
+          foreground: 'hsl(var(--muted-foreground))',
+        },
+        accent: {
+          DEFAULT: 'hsl(var(--accent))',
+          foreground: 'hsl(var(--accent-foreground))',
+        },
+        popover: {
+          DEFAULT: 'hsl(var(--popover))',
+          foreground: 'hsl(var(--popover-foreground))',
+        },
+        card: {
+          DEFAULT: 'hsl(var(--card))',
+          foreground: 'hsl(var(--card-foreground))',
+        },
+      },
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)',
+      },
+      keyframes: {
+        'accordion-down': {
+          from: { height: 0 },
+          to: { height: 'var(--radix-accordion-content-height)' },
+        },
+        'accordion-up': {
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: 0 },
+        },
+      },
+      animation: {
+        'accordion-down': 'accordion-down 0.2s ease-out',
+        'accordion-up': 'accordion-up 0.2s ease-out',
       },
     },
   },
-  plugins: [],
-} satisfies Config;
+  // eslint-disable-next-line global-require
+  plugins: [require('tailwindcss-animate')],
+};
