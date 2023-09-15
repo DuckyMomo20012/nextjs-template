08a908493377cbe19fbd54dd103170b70362ab2d
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-09-15 10:33:36 +0700

    fix: mismatch configs between MantineProvider and Tailwind

--- src/context/MantineProvider.tsx
+++ src/context/MantineProvider.tsx
@@ -5,78 +5,85 @@ import {
   Global,
   MantineTheme,
   DEFAULT_THEME as mantineDefaultTheme,
+  rem,
 } from '@mantine/core';
 import { useEffect, useState } from 'react';
+import twDefaultColors from 'tailwindcss/colors';
+import twDefaultTheme from 'tailwindcss/defaultTheme';
 
-// const convertBreakpoint = (breakpoint: ThemeType): MantineSizes => {
-//   const convertedBreakpoint = {} as MantineSizes;
-//   Object.keys(breakpoint).forEach((size) => {
-//     // NOTE: Have to convert 'px' to 'rem'
-//     convertedBreakpoint[size] = rem(breakpoint[size]);
-//   });
-//   return convertedBreakpoint;
-// };
+const convertBreakpoint = (breakpoint) => {
+  const convertedBreakpoint = {};
+  Object.keys(breakpoint).forEach((size) => {
+    // NOTE: Have to convert 'px' to 'rem'
+    convertedBreakpoint[size] = rem(breakpoint[size]);
+  });
+  return convertedBreakpoint;
+};
 
 // Override Mantine colors
-// const convertColor = (windiColors) => {
-//   const convertedColor = {} as MantineThemeColors;
-//   Object.keys(windiColors).forEach((color) => {
-//     if (color === 'lightBlue') {
-//       color = 'sky';
-//     } else if (color === 'warmGray') {
-//       color = 'stone';
-//     } else if (color === 'trueGray') {
-//       color = 'neutral';
-//     } else if (color === 'coolGray') {
-//       color = 'gray';
-//     } else if (color === 'blueGray') {
-//       color = 'slate';
-//     } else if (color === 'zink') {
-//       color = 'zinc';
-//     }
+const convertColor = (twColors) => {
+  const convertedColor = {};
+  Object.keys(twColors).forEach((color) => {
+    if (color === 'lightBlue') {
+      color = 'sky';
+    } else if (color === 'warmGray') {
+      color = 'stone';
+    } else if (color === 'trueGray') {
+      color = 'neutral';
+    } else if (color === 'coolGray') {
+      color = 'gray';
+    } else if (color === 'blueGray') {
+      color = 'slate';
+    } else if (color === 'zink') {
+      color = 'zinc';
+    }
 
-//     if (windiColors[color] instanceof Object) {
-//       convertedColor[color] = Object.values(windiColors[color]);
-//     }
-//   });
-//   // NOTE: WindiCSS dark color is too dark
-//   convertedColor.dark = mantineDefaultTheme.colors.dark;
+    if (twColors[color] instanceof Object) {
+      convertedColor[color] = Object.values(twColors[color]);
+    }
+  });
 
-//   return convertedColor;
-// };
+  return convertedColor;
+};
 
-// const convertFontSize = (fontSize): MantineSizes => {
-//   const convertedFontSize = {} as MantineSizes;
-//   Object.keys(fontSize).forEach((size) => {
-//     // NOTE: Don't have to convert 'rem' to 'px'
-//     convertedFontSize[size] = fontSize[size][0];
-//   });
-//   return convertedFontSize;
-// };
+const convertFontSize = (fontSize) => {
+  const convertedFontSize = {};
+  Object.keys(fontSize).forEach((size) => {
+    // NOTE: Don't have to convert 'rem' to 'px'
+    convertedFontSize[size] = fontSize[size][0];
+  });
+  return convertedFontSize;
+};
 
 const theme: MantineTheme = {
   ...mantineDefaultTheme,
   breakpoints: {
     ...mantineDefaultTheme.breakpoints,
+    ...convertBreakpoint(twDefaultTheme.screens), // TailwindCSS
   },
   colors: {
     ...mantineDefaultTheme.colors,
+    ...convertColor(twDefaultColors),
   },
   defaultRadius: 'md',
+  black: twDefaultColors.black as string,
+  white: twDefaultColors.white as string,
   primaryColor: 'blue',
   primaryShade: 7,
   fontSizes: {
     ...mantineDefaultTheme.fontSizes,
+    ...convertFontSize(twDefaultTheme.fontSize),
   },
   radius: {
     ...mantineDefaultTheme.radius,
-    // NOTE: WindiCSS border radius messed up with Mantine
-    // ...windiDefaultTheme.borderRadius,
+    // NOTE: TailwindCSS border radius messed up with Mantine
+    // ...twDefaultTheme.borderRadius,
   },
   lineHeight: mantineDefaultTheme.lineHeight,
   loader: 'oval',
   shadows: {
     ...mantineDefaultTheme.shadows,
+    ...twDefaultTheme.boxShadow,
   },
 };
 
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,35 @@
+import { DEFAULT_THEME as mantineDefaultTheme } from '@mantine/core';
 import type { Config } from 'tailwindcss';
+import twDefaultColors from 'tailwindcss/colors';
+
+// Don't override WindiCSS colors
+const convertColor = (mantineColors, windiColors) => {
+  const newColorPalette = {};
+  Object.keys(mantineColors).forEach((colorName) => {
+    if (windiColors[colorName] instanceof Object === false) {
+      const newColor = {};
+      mantineColors[colorName].forEach((_colorHex, index) => {
+        newColor[`${index * 100}`] = mantineColors[colorName][index];
+      });
+      newColor['50'] = newColor['0'];
+      delete newColor['0'];
+
+      newColorPalette[colorName] = newColor;
+    }
+  });
+
+  return newColorPalette;
+};
 
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ...convertColor(mantineDefaultTheme.colors, twDefaultColors),
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
