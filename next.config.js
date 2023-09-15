c2a995b4262434d06561ff786c73b307e91f7a4e
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-09-15 08:00:32 +0700

    refactor: migrate to tailwind

--- next.config.js
+++ next.config.js
@@ -1,12 +1,6 @@
-import WindiCSSWebpackPlugin from 'windicss-webpack-plugin';
-
 /** @type {import('next').NextConfig} */
 
 export default {
-  webpack: (config) => {
-    config.plugins.push(new WindiCSSWebpackPlugin());
-    return config;
-  },
   eslint: {
     ignoreDuringBuilds: true,
   },
--- src/context/MantineProvider.tsx
+++ src/context/MantineProvider.tsx
@@ -1,85 +1,72 @@
-import type { ColorScheme, MantineSizes } from '@mantine/core';
+import type { ColorScheme } from '@mantine/core';
 import {
   MantineProvider as BaseMantineProvider,
   ColorSchemeProvider,
   Global,
   MantineTheme,
   DEFAULT_THEME as mantineDefaultTheme,
-  rem,
 } from '@mantine/core';
 import { useEffect, useState } from 'react';
-import windiDefaultColors from 'windicss/colors';
-import windiDefaultTheme from 'windicss/defaultTheme';
-import type { DefaultColors } from 'windicss/types/config/colors';
-import type { DefaultFontSize, ThemeType } from 'windicss/types/interfaces';
-import type { MantineThemeColors } from '@/types/MantineThemeColors';
 
-const convertBreakpoint = (breakpoint: ThemeType): MantineSizes => {
-  const convertedBreakpoint = {} as MantineSizes;
-  Object.keys(breakpoint).forEach((size) => {
-    // NOTE: Have to convert 'px' to 'rem'
-    convertedBreakpoint[size] = rem(breakpoint[size]);
-  });
-  return convertedBreakpoint;
-};
+// const convertBreakpoint = (breakpoint: ThemeType): MantineSizes => {
+//   const convertedBreakpoint = {} as MantineSizes;
+//   Object.keys(breakpoint).forEach((size) => {
+//     // NOTE: Have to convert 'px' to 'rem'
+//     convertedBreakpoint[size] = rem(breakpoint[size]);
+//   });
+//   return convertedBreakpoint;
+// };
 
 // Override Mantine colors
-const convertColor = (windiColors: DefaultColors) => {
-  const convertedColor = {} as MantineThemeColors;
-  Object.keys(windiColors).forEach((color) => {
-    if (color === 'lightBlue') {
-      color = 'sky';
-    } else if (color === 'warmGray') {
-      color = 'stone';
-    } else if (color === 'trueGray') {
-      color = 'neutral';
-    } else if (color === 'coolGray') {
-      color = 'gray';
-    } else if (color === 'blueGray') {
-      color = 'slate';
-    } else if (color === 'zink') {
-      color = 'zinc';
-    }
+// const convertColor = (windiColors) => {
+//   const convertedColor = {} as MantineThemeColors;
+//   Object.keys(windiColors).forEach((color) => {
+//     if (color === 'lightBlue') {
+//       color = 'sky';
+//     } else if (color === 'warmGray') {
+//       color = 'stone';
+//     } else if (color === 'trueGray') {
+//       color = 'neutral';
+//     } else if (color === 'coolGray') {
+//       color = 'gray';
+//     } else if (color === 'blueGray') {
+//       color = 'slate';
+//     } else if (color === 'zink') {
+//       color = 'zinc';
+//     }
 
-    if (windiColors[color] instanceof Object) {
-      convertedColor[color] = Object.values(windiColors[color]);
-    }
-  });
-  // NOTE: WindiCSS dark color is too dark
-  convertedColor.dark = mantineDefaultTheme.colors.dark;
+//     if (windiColors[color] instanceof Object) {
+//       convertedColor[color] = Object.values(windiColors[color]);
+//     }
+//   });
+//   // NOTE: WindiCSS dark color is too dark
+//   convertedColor.dark = mantineDefaultTheme.colors.dark;
 
-  return convertedColor;
-};
+//   return convertedColor;
+// };
 
-const convertFontSize = (fontSize: {
-  [key: string]: DefaultFontSize;
-}): MantineSizes => {
-  const convertedFontSize = {} as MantineSizes;
-  Object.keys(fontSize).forEach((size) => {
-    // NOTE: Don't have to convert 'rem' to 'px'
-    convertedFontSize[size] = fontSize[size][0];
-  });
-  return convertedFontSize;
-};
+// const convertFontSize = (fontSize): MantineSizes => {
+//   const convertedFontSize = {} as MantineSizes;
+//   Object.keys(fontSize).forEach((size) => {
+//     // NOTE: Don't have to convert 'rem' to 'px'
+//     convertedFontSize[size] = fontSize[size][0];
+//   });
+//   return convertedFontSize;
+// };
 
 const theme: MantineTheme = {
   ...mantineDefaultTheme,
   breakpoints: {
     ...mantineDefaultTheme.breakpoints,
-    ...convertBreakpoint(windiDefaultTheme.screens), // WindiCSS
   },
   colors: {
     ...mantineDefaultTheme.colors,
-    ...convertColor(windiDefaultColors),
   },
   defaultRadius: 'md',
-  black: windiDefaultColors.black as string,
-  white: windiDefaultColors.white as string,
   primaryColor: 'blue',
   primaryShade: 7,
   fontSizes: {
     ...mantineDefaultTheme.fontSizes,
-    ...convertFontSize(windiDefaultTheme.fontSize),
   },
   radius: {
     ...mantineDefaultTheme.radius,
@@ -90,7 +77,6 @@ const theme: MantineTheme = {
   loader: 'oval',
   shadows: {
     ...mantineDefaultTheme.shadows,
-    ...windiDefaultTheme.boxShadow,
   },
 };
 
