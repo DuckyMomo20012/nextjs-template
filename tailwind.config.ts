4341e6e709942f8a4d8d803546edc584b4624fa2
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-09-21 09:41:26 +0700

    fix: mantine & tailwind color mismatch

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,5 @@
-import { DEFAULT_THEME as mantineDefaultTheme } from '@mantine/core';
 import type { Config } from 'tailwindcss';
 import twDefaultColors from 'tailwindcss/colors';
-
-// Don't override WindiCSS colors
-const convertColor = (mantineColors, windiColors) => {
-  const newColorPalette = {};
-  Object.keys(mantineColors).forEach((colorName) => {
-    if (windiColors[colorName] instanceof Object === false) {
-      const newColor = {};
-      mantineColors[colorName].forEach((_colorHex, index) => {
-        newColor[`${index * 100}`] = mantineColors[colorName][index];
-      });
-      newColor['50'] = newColor['0'];
-      delete newColor['0'];
-
-      newColorPalette[colorName] = newColor;
-    }
-  });
-
-  return newColorPalette;
-};
 
 export default {
   content: ['./src/**/*.{js,ts,jsx,tsx,mdx}'],
@@ -27,7 +7,8 @@ export default {
   theme: {
     extend: {
       colors: {
-        ...convertColor(mantineDefaultTheme.colors, twDefaultColors),
+        dark: twDefaultColors.slate,
+        grape: twDefaultColors.fuchsia,
       },
     },
     screens: {
