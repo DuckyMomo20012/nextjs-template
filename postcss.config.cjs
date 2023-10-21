d890a70878762aa0bd88e151e7fb7e40d512de47
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-10-21 21:06:10 +0700

    chore: update ts configs
    chore: remove postcss configs
    
    chore: update config files

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -3,15 +3,6 @@ module.exports = {
     tailwindcss: {},
     autoprefixer: {},
     'postcss-preset-mantine': {},
-    'postcss-simple-vars': {
-      variables: {
-        'mantine-breakpoint-xs': '30em',
-        'mantine-breakpoint-sm': '40em',
-        'mantine-breakpoint-md': '48em',
-        'mantine-breakpoint-lg': '64em',
-        'mantine-breakpoint-xl': '80em',
-        'mantine-breakpoint-2xl': '96em',
-      },
-    },
+    'postcss-simple-vars': {},
   },
 };
