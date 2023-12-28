539cf51db40cbe2b3d706e5903b3b176572899bd
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-12-28 11:01:03 +0700

    fix(api): update next-auth demo config

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,12 +1,13 @@
-import { sha1 } from 'hash-wasm';
+import { bcryptVerify } from 'hash-wasm';
 import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 
-function getOneUser() {
+function getOneUser(email: string) {
   const user = {
-    email: 'tienvinh@gmail.com',
+    id: '1',
+    email,
     name: 'Dương Tiến Vinh',
-    password: '7110eda4d09e062aa5e4a390b0a572ac0d2c0220', // 1234
+    password: '$2a$10$vOJLhI.jeFrIB0imYb8OMu5yyhGxHyjsElagxR2ESMX5nKsHW0rxO', // password is 'password'
   };
 
   return Promise.resolve({ ...user, password: user.password });
@@ -28,20 +29,30 @@ export default NextAuth({
         },
       },
       async authorize(credentials) {
+        if (!credentials) return null;
+
         // Inputs from login form
         const { email, password } = credentials;
-        const hashPassword = await sha1(password);
-        const user = await getOneUser();
-        if (user && user.password === hashPassword) {
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          return user as any;
+
+        const user = await getOneUser(email);
+
+        if (user) {
+          const isVerified = await bcryptVerify({
+            password,
+            hash: user.password,
+          });
+
+          if (!isVerified) return null;
+
+          return user;
         }
+
         return null;
       },
     }),
   ],
   pages: {
-    signIn: '/auth/login',
+    signIn: '/auth/sign-in',
   },
   events: {
     signIn(message) {
