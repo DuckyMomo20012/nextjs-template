175dc4ae7641215ab00ebbecbcb36cbc2a5f828c
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-12-28 21:47:07 +0700

    refactor: migrate next-auth to app dir

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -3,21 +3,45 @@
 import '@/styles/globals.css';
 
 import { Notifications } from '@mantine/notifications';
+import { type GetServerSidePropsContext } from 'next';
+import { type Session } from 'next-auth';
 import NextNProgress from 'nextjs-progressbar';
-import { MantineProvider, QueryProvider, ReduxProvider } from '@/context/index';
+import {
+  MantineProvider,
+  QueryProvider,
+  ReduxProvider,
+  SessionProvider,
+} from '@/context/index';
+import { auth } from '@/lib/auth';
 
-const Providers = async ({ children }: { children?: React.ReactNode }) => {
+const Providers = ({
+  children,
+  session,
+}: {
+  children?: React.ReactNode;
+  session?: Session;
+}) => {
   return (
     <ReduxProvider>
-      <QueryProvider>
-        <MantineProvider>
-          <NextNProgress />
-          <Notifications />
-          {children}
-        </MantineProvider>
-      </QueryProvider>
+      <SessionProvider session={session}>
+        <QueryProvider>
+          <MantineProvider>
+            <NextNProgress />
+            <Notifications />
+            {children}
+          </MantineProvider>
+        </QueryProvider>
+      </SessionProvider>
     </ReduxProvider>
   );
 };
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await auth(context.req, context.res);
+
+  return {
+    props: { session },
+  };
+}
 
 export default Providers;
--- src/context/index.ts
+++ src/context/index.ts
@@ -2,3 +2,4 @@ export * from './AuthGuard';
 export * from './MantineProvider';
 export * from './QueryProvider';
 export * from './ReduxProvider';
+export * from './SessionProvider';
