97171d2183d0dae959d8b52c3f919c2165044530
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-12-28 17:40:48 +0700

    refactor(web): update sign in & sign up page

---
+++ src/pages/auth/sign-in.tsx
@@ -0,0 +1,33 @@
+import { Card, Center, Stack, Text } from '@mantine/core';
+import Link from 'next/link';
+import { SignInForm } from '@/components/forms/SignInForm';
+
+const SignIn = () => {
+  return (
+    <>
+      <Center className="h-full w-full flex-1">
+        <Card className="w-full max-w-md" withBorder>
+          <Stack gap="lg">
+            <Text fw="700" fz="xl">
+              Sign in
+            </Text>
+
+            <SignInForm />
+
+            <Text>
+              Do not have an account?{' '}
+              <Link
+                className="text-inherit no-underline hover:underline"
+                href="/auth/sign-up"
+              >
+                Sign up
+              </Link>
+            </Text>
+          </Stack>
+        </Card>
+      </Center>
+    </>
+  );
+};
+
+export default SignIn;
