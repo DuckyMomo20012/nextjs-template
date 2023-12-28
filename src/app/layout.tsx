ebdf89fffbafaa5105c09c5ea13b26f86c936d86
Dương Tiến Vinh <64480713+DuckyMomo20012@users.noreply.github.com>
2023-12-28 22:27:17 +0700

    chore: remove unused fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,10 +19,6 @@ const RootLayout = ({ children }: { children: React.ReactNode }) => {
           href="https://fonts.gstatic.com"
           rel="preconnect"
         />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&family=Quicksand:wght@700&family=Space+Mono:ital,wght@0,400;0,700;1,400;1,700&display=swap"
-          rel="stylesheet"
-        />
       </head>
       <body>
         <Providers>{children}</Providers>
